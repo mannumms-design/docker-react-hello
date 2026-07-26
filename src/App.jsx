23ba555af35dd1ecb0e738fa4314be237bc6b24b
ManmohanTomar <mannu.mms@gmail.com>
2026-07-27 00:23:50 +0530

    Initial React Kubernetes Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
+      <h1>Hello World from React inside Docker</h1>
+      <p>This is my first containerized React application.</p>
+    </div>
+  );
+}
+
+export default App;
